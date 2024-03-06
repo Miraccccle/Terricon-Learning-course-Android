158fa987229baba3533d44bc18767140658e2336
Miraccccle <mirasgaziz3@gmail.com>
2024-03-06 17:00:54 +0600

    Alert dialog

--- app/src/main/java/com/example/allertdialog/MainActivity.kt
+++ app/src/main/java/com/example/allertdialog/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.allertdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import com.example.allertdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        val builder = AlertDialog.Builder(this).create()
+        val view = layoutInflater.inflate(R.layout.custom_dialog, null)
+        builder.setView(view)
+        val dismissBtn = view.findViewById<Button>(R.id.dismissBtn)
+        val allowBtn = view.findViewById<Button>(R.id.allowBtn)
+        dismissBtn.setOnClickListener {
+            builder.dismiss()
+        }
+        allowBtn.setOnClickListener {
+            builder.dismiss()
+        }
+        builder.setCanceledOnTouchOutside(false)
+        binding!!.activateModal.setOnClickListener {
+            builder.show()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
